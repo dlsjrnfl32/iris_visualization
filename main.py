546aa259afc138a6b9dbf1a20172de5f26904b55
JH LEE <dlsjrnfl@naver.com>
2023-01-11 09:30:54 +0900

    1 commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+feature_names = iris.feature_names
+species = iris.target_names
+
+X, y = iris.data, iris.target
+
+X = pd.DataFrame(X, columns=feature_names)
+y = pd.Series(y, name='species')
+
+iris_df = pd.concat([X, y], axis=1)
+save_dir = 'dataset'
+file_name = 'iris_dataset.csv'
+os.makedirs(save_dir, exist_ok=True)
+iris_df.to_csv(os.path.join(save_dir, file_name))
+
+species_idx_df = pd.Series(species)
+file_name = 'species_idx.csv'
+species_idx_df.to_csv(os.path.join(save_dir, file_name),
+                      header=False)
+
+iris_df[iris_df.species == 0].hist()
+iris_df[iris_df.species == 1].hist()
+iris_df[iris_df.species == 2].hist()
+
+
